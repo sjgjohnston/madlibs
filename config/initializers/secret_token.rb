9074e8e581dfa65cc1e9d650ed45fc4d70267c46
sjgjohnston <johnston.sjg@gmail.com>
2013-10-30 08:39:05 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Madlibs::Application.config.secret_token = 'a5fe0abbb7de5f52086aef4c9f3f85a6880dcdc24b2327d8dcf6d654281e1ae2fba7cb9131c0b7a9c0551b0f1d1eec384580adf5191640195fbed6ff520e6fff'
